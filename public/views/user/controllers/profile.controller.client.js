18e7188e8509b36f16d422e751506babf5d74844
wangluyao1 <luyaowang1@gmail.com>
2017-08-11 17:15:43 -0400

    views: user templates and controller

---
+++ public/views/user/controllers/profile.controller.client.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Luyao on 7/21/2017.
+ */
+(function () {
+    angular
+        .module("WebAppMaker")
+        .controller("ProfileController", ProfileController)
+
+    function ProfileController(userService, $location, user) {
+        var model = this;
+        //model.userId = $routeParams["uid"];
+        model.userId = user._id;
+
+        model.updateUser = updateUser;
+        model.unregister = unregister;
+
+        function init() {
+            //model.user = userService.findUserById(model.userId);
+            var promise = userService.findUserById(model.userId);
+            promise.then(function (response) {
+                model.user = response.data;
+            })
+
+        }
+
+        init();
+
+        function updateUser(user) {
+            var promise = userService.updateUser(model.userId, user);
+            promise.then(function (response) {
+            });
+        }
+
+        function unregister() {
+            userService.deleteUser(model.userId)
+                .then(function (response) {
+                    console.log(response.status);
+                    if (response.status === 200) {
+                        $location.url("/login");
+                    }
+                });
+
+        }
+    }
+})();
