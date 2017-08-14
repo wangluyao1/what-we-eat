2bfd66cc95e5e6a9fb35749521c720ca39e86a7c
wangluyao1 <luyaowang1@gmail.com>
2017-08-14 01:09:31 -0400

    plate server service

--- project/service/plate.service.server.js
+++ project/service/plate.service.server.js
@@ -4,3 +4,53 @@
 var app = require('../../express');
 var plateModel = require("../model/plate/plate.model.server");
 
+app.post("/api/plate",createPlate);
+app.get("/api/plate/:pId",findPlateById);
+app.put("/api/plate/:pId",updatePlate);
+app.delete("/api/plate/:pId",deletePlate);
+
+function createPlate(req,res) {
+    var newPlate = req.body;
+    return plateModel
+        .createPlate(newPlate)
+        .then(function (status) {
+            res.json(status);
+        },function (err) {
+            res.sendStatus(500).send(err);
+        });
+}
+
+function findPlateById(req,res) {
+    var plateId = req.params.pId;
+    return plateModel
+        .findPlateById(plateId)
+        .then(function (plate) {
+            res.json(plate);
+        },function (err) {
+            res.sendStatus(404).send(err);
+        })
+}
+
+
+function updatePlate(req,res) {
+    var plateId = req.params.pId;
+    var newPlate = req.body.plate;
+    return plateModel
+        .updatePlate(plateId,newPlate)
+        .then(function (plate) {
+            res.json(plate);
+        },function (err) {
+            res.sendStatus(500).send(err);
+        });
+}
+
+function deletePlate(req,res) {
+    var plateId = req.params.pId;
+    return plateModel
+        .deletePlate(plateId)
+        .then(function (status) {
+            res.json(status);
+        },function (err) {
+            res.sendStatus(500).send(err);
+        });
+}
