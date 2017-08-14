665875e6318ba59eea974989771c4eab8790485a
wangluyao1 <luyaowang1@gmail.com>
2017-08-14 01:04:20 -0400

    restaurant server service

--- project/service/plate.service.server.js
+++ project/service/plate.service.server.js
@@ -1,3 +1,6 @@
 /**
  * Created by Luyao on 8/14/2017.
  */
+var app = require('../../express');
+var plateModel = require("../model/plate/plate.model.server");
+
--- project/service/restaurant.service.server.js
+++ project/service/restaurant.service.server.js
@@ -1,3 +1,67 @@
 /**
  * Created by Luyao on 8/14/2017.
  */
+var app = require('../../express');
+var restaurantModel = require("../model/restaurant/restaurant.model.server");
+
+app.post("/api/restaurant",createRestaurant);
+app.get("/api/restaurant/:rId",findResById);
+app.get("/api/restaurant/:rId/menu",findMenuByResId);
+app.put("/api/restaurant/:rId",updateRestaurant);
+app.delete("/api/restaurant/:rId",deleteRestaurant);
+
+function createRestaurant(req,res) {
+    var newRes = req.body;
+    return restaurantModel
+        .createPlate(newRes)
+        .then(function (status) {
+            res.json(status);
+        },function (err) {
+            res.sendStatus(500).send(err);
+        });
+}
+
+function findResById(req,res) {
+    var resId = req.params.rId;
+    return restaurantModel
+        .findRestaurantById(resId)
+        .then(function (restaurant) {
+            res.json(restaurant);
+        },function (err) {
+            res.sendStatus(404).send(err);
+        })
+}
+
+function findMenuByResId(req,res) {
+    var resId = req.params.rId;
+    return restaurantModel
+        .findMenu(resId)
+        .then(function (menu) {
+            res.json(menu);
+        },function (err) {
+            res.sendStatus(404).send(err);
+        });
+}
+
+function updateRestaurant(req,res) {
+    var resId = req.params.rId;
+    var newRes = req.body.restaurant;
+    return restaurantModel
+        .updateRestaurant(resId,newRes)
+        .then(function (restaurant) {
+            res.json(restaurant);
+        },function (err) {
+            res.sendStatus(500).send(err);
+        });
+}
+
+function deleteRestaurant(req,res) {
+    var resId = req.params.rId;
+    return restaurantModel
+        .deleteRestaurant(resId)
+        .then(function (status) {
+            res.json(status);
+        },function (err) {
+            res.sendStatus(500).send(err);
+        });
+}
